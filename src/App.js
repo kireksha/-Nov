897ad6cefbdeef485eaa8fed7772882dac031200
Drozdi <drozdi-klim@yandex.ru>
2024-11-23 19:02:07 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { XAccordionTab } from './components/ui/accordion';
-import { XProgress } from './components/ui/progress';
+import { XDropdown } from './components/ui';
 
 export const App = () => {
 	const [type, setType] = useState('bar');
@@ -10,56 +9,9 @@ export const App = () => {
 	return (
 		<>
 			<div className="App">
-				<XAccordionTab header="Обо мне">
-					<p>
-						content content content content content content content content
-						content content content content content content content content
-						content content content content content content content content
-						content content content content content content content content
-						content content content content content content content content
-						content content
-					</p>
-				</XAccordionTab>
-				<XAccordionTab header="Мой прогресс" style={{ marginTop: '8px' }}>
-					<div style={{ display: 'flex', justifyContent: 'space-around' }}>
-						<XProgress
-							type="circle"
-							value={80}
-							size={128}
-							thickness={10}
-							color="negative"
-						>
-							HTML
-						</XProgress>
-						<XProgress
-							type="circle"
-							value={90}
-							size={128}
-							thickness={10}
-							color="secondary"
-						>
-							CSS
-						</XProgress>
-						<XProgress
-							type="circle"
-							value={75}
-							size={128}
-							thickness={10}
-							color="warning"
-						>
-							JS
-						</XProgress>
-						<XProgress
-							type="circle"
-							value={95}
-							size={128}
-							thickness={10}
-							color="info"
-						>
-							React
-						</XProgress>
-					</div>
-				</XAccordionTab>
+				<XDropdown label="Cl">
+					<p>dlkfhgdflkgjfdkgl</p>
+				</XDropdown>
 			</div>
 		</>
 	);
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,3 +1,4 @@
 export * from './accordion';
+export * from './dropdown';
 export * from './progress';
 export * from './spinner';
