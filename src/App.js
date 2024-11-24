41caea172b5d2b70f88a83d616853ffd6ab59103
Drozdi <drozdi-klim@yandex.ru>
2024-11-24 12:44:47 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import { XDropdown } from './components/ui'
 import { Route, Routes } from 'react-router-dom';
-import { Header, Footer, Badge } from './components';
-import {MainPage } from './Pages';
 import styled from 'styled-components';
+import { Badge, Footer, Header } from './components';
+import { MainPage } from './Pages';
 
 const Content = styled.div`
 	margin: 0 auto;
@@ -11,20 +10,17 @@ const Content = styled.div`
 `;
 
 export const AppContainer = ({ className }) => {
-  const [type, setType] = useState('bar');
+	const [type, setType] = useState('bar');
 	const [value, setValue] = useState(33);
 	const [size, setSize] = useState(85);
 	const [thickness, setThickness] = useState(16);
 	return (
 		<div className={className}>
-			<XDropdown>
-				<p>dlkfhgdflkgjfdkgl</p>
-			</XDropdown>
 			<Header />
 			<Content>
 				<h2>162 ГРУППА БУДУЩИХ ВЫПУСКНИКОВ RESULT UNIVERSITY</h2>
 				<Routes>
-					<Route path="/" element={<MainPage/>} />
+					<Route path="/" element={<MainPage />} />
 					<Route
 						path="/coders/:id"
 						element={<div>Открытая страница программиста по id</div>}
